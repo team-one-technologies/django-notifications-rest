69e164006b1bd212d934f3e05dd1bbfadfd3cf4f
opensworup <40563798+opensworup@users.noreply.github.com>
2023-03-24 12:40:54 +0545

    added profile_picture field in userserializer

--- notifications_rest/serializers.py
+++ notifications_rest/serializers.py
@@ -10,10 +10,23 @@ UserModel = get_user_model()
 
 class UserSerializer(ModelSerializer):
     id = serializers.IntegerField()
+    profile_picture = serializers.SerializerMethodField(method_name="get_profile_picture", required=False)
+
+    def get_profile_picture(self, obj):
+        # check if the CustomUser instance has a value for the profile_picture field
+        if obj.profile_picture:
+            print(obj.profile_picture)
+            # return the value of the profile_picture field
+            request = self.context.get("request")
+            profile_picture_url = obj.profile_picture.url
+            return request.build_absolute_uri(profile_picture_url)
+        # return the default image if the CustomUser instance does not have a value for the profile_picture field
+        else:
+            return 'https://insurestag.plantsat.com/media/avatar.jpg'
 
     class Meta:
         model = UserModel
-        fields = ['id', ]
+        fields = ['id', 'profile_picture']
 
 
 class ContentTypeSerializer(ModelSerializer):
@@ -45,14 +58,14 @@ class NotificationSerializer(ModelSerializer):
     deleted = serializers.BooleanField()
     emailed = serializers.BooleanField()
     timesince = serializers.SerializerMethodField(read_only=True, method_name="get_timesince")
-    
+
     def get_timesince(self, obj):
         return timesince_(obj.timestamp, None)
 
     class Meta:
         model = Notification
         fields = ['id', 'recipient', 'actor', 'target', 'verb', 'level', 'description', 'unread', 'public', 'deleted',
-                  'emailed', 'timestamp','timesince']
+                  'emailed', 'timestamp', 'timesince']
 
     def create(self, validated_data):
         recipient_data = validated_data.pop('recipient')
